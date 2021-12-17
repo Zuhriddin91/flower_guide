a3fc5ddfcac723a051041bfd0c543bd1c8a9aeda
Zuhriddin91 <turoni1991@gmail.com>
2021-12-17 17:20:17 +0500

    test 2

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -2,4 +2,10 @@ import 'package:flutter/painting.dart';
 
 class MyColors {
   static const Color primary = Color(0xff51B1A6);
+  static const Color secondary = Color(0xff5E5BD9);
+  static const Color dark = Color(0xff24243F);
+  static const Color text = Color(0xff5a5a75);
+  static const Color subtitleText = Color(0xff8C8CA1);
+  static const Color accent = Color(0xffF5F7FA);
+  static const Color light = Color(0xffFAFCFE);
 }
