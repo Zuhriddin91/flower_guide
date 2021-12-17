bbbcb4ea81cb4e190d0f11533311a21c78766f22
Zuhriddin91 <turoni1991@gmail.com>
2021-12-17 17:40:15 +0500

    test 3

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Hello World !"),
+        child: Text("Hello !"),
       ),
       bottomNavigationBar: MyBottomBar(),
     );
