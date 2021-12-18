8e2ab278ef31dd00f22c797fd745605bd279c2c3
BahromjonPolat <itjunior.uz@gmail.com>
2021-12-18 11:42:46 +0500

    icon path'lari yozildi

--- lib/core/constants/icons.dart
+++ lib/core/constants/icons.dart
@@ -1,5 +1,20 @@
-
 class MyIcons {
   static String search = 'assets/icons/search.svg';
-
+  static String bell = 'assets/icons/bell.svg';
+  static String check = 'assets/icons/check.svg';
+  static String chevronLeft = 'assets/icons/chevron_left.svg';
+  static String close = 'assets/icons/close.svg';
+  static String dropLed = 'assets/icons/dropled.svg';
+  static String favouriteActive = 'assets/icons/favourite_active.svg';
+  static String favouriteOutlined = 'assets/icons/favourite_outlined.svg';
+  static String home = 'assets/icons/home.svg';
+  static String leaf = 'assets/icons/leaf.svg';
+  static String library = 'assets/icons/library.svg';
+  static String messageCircle = 'assets/icons/message_circle.svg';
+  static String minus = 'assets/icons/minus.svg';
+  static String plus = 'assets/icons/plus.svg';
+  static String shopping = 'assets/icons/shopping.svg';
+  static String trash = 'assets/icons/trash.svg';
+  static String user = 'assets/icons/user.svg';
+  static String warning = 'assets/icons/warning.svg';
 }
