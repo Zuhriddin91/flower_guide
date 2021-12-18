582151534802adfc0ab618d86448e5effe6ddf01
BahromjonPolat <itjunior.uz@gmail.com>
2021-12-18 15:59:50 +0500

    .....

--- lib/core/constants/imports.dart
+++ lib/core/constants/imports.dart
@@ -4,6 +4,8 @@ export 'package:flutter/services.dart';
 export 'package:flower_guide/provider/bottom_bar_provider.dart';
 export 'package:flower_guide/provider/text_field_provider.dart';
 export 'package:flower_guide/provider/page_view_provider.dart';
+export 'package:flower_guide/provider/home_provider.dart';
+export 'package:flower_guide/provider/tab_bar_provider.dart';
 
 // Screens
 export 'package:flower_guide/view/screens/home_page.dart';
@@ -14,10 +16,13 @@ export 'package:flower_guide/view/widgets/my_bottom_nav_bar.dart';
 export 'package:flower_guide/view/widgets/my_text_field.dart';
 export 'package:flower_guide/view/widgets/my_elevated_button.dart';
 export 'package:flower_guide/view/widgets/detail_page_page_view.dart';
+export 'package:flower_guide/view/widgets/my_tab_bar.dart';
+
 // Packages
 export 'package:provider/provider.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:carousel_slider/carousel_slider.dart';
+export 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 // Constants
 export 'package:flower_guide/core/constants/colors.dart';
@@ -25,4 +30,4 @@ export 'package:flower_guide/core/constants/icons.dart';
 export 'package:flower_guide/core/constants/size_config.dart';
 
 // Themes
-import 'package:flower_guide/core/components/app_bar_theme.dart';
+export 'package:flower_guide/core/components/app_bar_theme.dart';
---
+++ lib/view/screens/favourites_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flower_guide/core/constants/imports.dart';
+
+class MyFavouritesPage extends StatelessWidget {
+  const MyFavouritesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
